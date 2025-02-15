f4749e247aae9cd0ded8f3615988681042ed3a2b
James Jahner <jitone1@gmail.com>
2025-02-15 12:11:30 -0600

    added a react from another project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, createRoutesFromElements, RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+const App = () => {
+    // Enables navigation in the app
+  return <RouterProvider router={router} />;
+};
+
+export default App;
+
+
+
