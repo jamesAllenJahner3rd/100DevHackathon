d94c40461156b81467f5015877c82cf414752ec5
James Jahner <jitone1@gmail.com>
2025-02-16 21:40:19 -0600

    beeps alot.

--- src/components/ScanFoodUPC.jsx
+++ src/components/ScanFoodUPC.jsx
@@ -3,7 +3,7 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const ScanFoodUPC = () => {
   const [data, setData] = React.useState("Not Found");
-
+const  beepSound = new Audio("../src/assets/sounds/beep.ogg");
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-full max-w-lg mx-auto">
@@ -11,7 +11,10 @@ const ScanFoodUPC = () => {
           width={500}
           height={500}
           onUpdate={(err, result) => {
-            if (result) setData(result.text);
+            if (result){
+               setData(result.text)
+              beepSound.play().catch(err => console.error('Error playing beep:', err));
+    } 
             else setData("Not Found");
           }}
         />
